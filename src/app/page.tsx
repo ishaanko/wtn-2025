ad46a2ccadac1569459a7c5177cb9195cb6cf851
Inesh Dey <ineshdey@hotmail.com>
2025-04-15 15:32:19 -0700

    spacing fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -787,7 +787,7 @@ export default function Home() {
             <OpioidOverdoseChart />
           </motion.div>
             <motion.p
-              className="text-3xl font-bold pt-8 text-center pb-8"
+              className="text-3xl font-bold pt-24 text-center pb-8"
               initial={{ opacity: 0, y: -20 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.2 }}
